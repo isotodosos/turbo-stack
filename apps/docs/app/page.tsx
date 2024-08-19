6c69fea6c69e9a3eb4730f082bd23bfa7cf30968
Ivan <ivan.sotodosos@flat101.es>
2024-08-19 16:28:52 +0200

    Changed  'Contenido' por 'Main'

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           </ul>
         </nav>
       </header>
-      <main className={styles.main}>Contenido de la Home</main>
+      <main className={styles.main}>Main de la Home</main>
       <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
