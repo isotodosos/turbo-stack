e4d3ba6692a801c7ecc2e0fe93c569201297d24a
Ivan <ivan.sotodosos@flat101.es>
2024-08-30 16:33:38 +0200

    Feature: -Created login and page protected

--- apps/supa-next/app/protected/page.tsx
+++ apps/supa-next/app/protected/page.tsx
@@ -1,9 +1,8 @@
 import DeployButton from "@/components/DeployButton";
 import AuthButton from "@/components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
 import { redirect } from "next/navigation";
+import Students from "@/components/Students";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -19,7 +18,7 @@ export default async function ProtectedPage() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="w-full">
-        <div className="py-6 font-bold bg-purple-950 text-center">
+        <div className="py-6 font-bold bg-purple-950 text-center text-white">
           This is a protected page that you can only see as an authenticated
           user
         </div>
@@ -30,28 +29,9 @@ export default async function ProtectedPage() {
           </div>
         </nav>
       </div>
-
-      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
-        <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <FetchDataSteps />
-        </main>
+      <div>
+        <Students />
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
